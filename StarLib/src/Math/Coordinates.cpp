49bb65202f32f0642b77f77ae3bcee5d29591084
perkun <g.dudzinski@protonmail.com>
2021-11-15 09:14:08 +0100

    ...whatever

--- StarLib/src/Math/Coordinates.cpp
+++ StarLib/src/Math/Coordinates.cpp
@@ -3,20 +3,27 @@
 namespace StarLib
 {
 
+static double  G1n (282.859481208*M_PI/180.0);
+static double  G2n (62.871748806*M_PI/180.0);
+static double  G3n (327.06808143*M_PI/180.0);
+
+
 Vec3 galactic_to_equatorial(Vec3 vec)
 {
-	double ngp_ra = 192.85 * UNITS::DEG;
-	double ngp_dec = 27.13 * UNITS::DEG;
-	double ncp_long = 122.93 * UNITS::DEG;
+	Vec3 result = rotate_z(vec, -G3n);
+	result = rotate_x(result, -G2n);
+	result = rotate_z(result, -G1n);
 
-	// vec to longitude, latitude
-	double gal_long = atan2(vec.y, vec.x);
-	double ga_lat = asin(vec.z / vec.length());
+	return result;
+}
 
-	double dec = asin( sin(ngp_dec)  );
+Vec3 equatorial_to_galactic(Vec3 vec)
+{
+	Vec3 result = rotate_z(vec, G1n);
+	result = rotate_x(result, G2n);
+	result = rotate_z(result, G3n);
 
-
-	return Vec3();
+	return result;
 }
 
 }
--- StarLib/src/Math/Coordinates.h
+++ StarLib/src/Math/Coordinates.h
@@ -10,6 +10,8 @@ namespace StarLib
 {
 
 Vec3 galactic_to_equatorial(Vec3 vec);
+Vec3 equatorial_to_galactic(Vec3 vec);
+Vec3 cartesian_to_spherical(Vec3 vec);
 
 }
 
